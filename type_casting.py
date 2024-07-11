1eccabc2bb29505108833893bea49e6553f7d17c
RavinduPerera <it22064622@my.sliit.lk>
2024-07-11 16:19:24 +0530

    typecasting updated

---
+++ type_casting.py
@@ -0,0 +1,22 @@
+# chaning data types to another type
+
+x = 1
+y = 2.0
+z = '6'
+
+# converting into int
+y  = int(y)
+z = int(z)
+
+# converting into float
+x = float(x)
+z = float(z)
+
+# converting into string
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(z)
+print(z*2)
