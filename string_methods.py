a1e56d8bea19b9979cd31646465dc508194f70d8
RavinduPerera <it22064622@my.sliit.lk>
2024-07-11 16:06:44 +0530

    useful string methods updated

--- string_methods.py
+++ string_methods.py
@@ -5,3 +5,27 @@ print(len(name))
 
 # character finder - find()
 print(name.find('r'))
+
+# capiatalize first character - capitalize()
+print(name.capitalize())
+
+# full uppercase - upper()
+print(name.upper())
+
+# full lowercase - lower()
+print(name.lower())
+
+# identify digits or not - isdigit()
+print(name.isdigit())
+
+# check the string contains only alphabetical letters - isalpha()
+print(name.isalpha())
+
+# count how many charaters are in the string ech - count('char')
+print(name.count('r'))
+
+# replacing chars - replace('replace_char', 'replacing_with')
+print(name.replace('v', 'a'))
+
+# multiplying variable
+print(name*3)
