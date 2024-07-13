2a43b4013557997bd7a61036b5a7be876202d1d7
RavinduPerera <it22064622@my.sliit.lk>
2024-07-13 17:55:18 +0530

    user input 2 done

--- user_inputs.py
+++ user_inputs.py
@@ -1,5 +1,9 @@
-# input()
+# inputs 
+name = input('enter your name here: ')
+age = int(input('enter your age: ')) # typecasting 1 - int convert
+height = float(input('enter your height: ')) # float convert
 
-name = input('What is your name: ')
-
-print('Hello, '+name)
+print('')
+print('hello '+name+ ', welcome to python.')
+print('You are '+str(age)+' years old.') # typecasting 2 - int convert
+print('You are '+str(height)+'inch tall') # float convert 
