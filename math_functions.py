716610f46f290e8b517f1882fa81b0cb4faea8d6
RavinduPerera <it22064622@my.sliit.lk>
2024-07-13 18:12:01 +0530

    math functions done

---
+++ math_functions.py
@@ -0,0 +1,16 @@
+# math_functions
+import math
+
+pi  = 3.14
+x = 3
+y = 4
+z = 8
+
+print(round(pi)) # rounding to the nearest number
+print(math.ceil(pi)) # roundig to the upper number
+print(math.floor(pi)) # rounding to the lower number
+print(abs(pi)) # absalute values (negetives make postives)
+print(pow(pi, 2)) # 2 to the power (2 can change)
+print(math.sqrt(pi)) # squre root fucntions
+print(max(x, y, z)) # largest number
+print(min(x, y, z)) # smallest number
