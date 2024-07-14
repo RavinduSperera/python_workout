12dafe1e0b57650ce65cb2142fedd3114c779704
RavinduPerera <it22064622@my.sliit.lk>
2024-07-14 10:14:57 +0530

    string slicing part 1 done

---
+++ string_slicing.py
@@ -0,0 +1,13 @@
+#  slicing - create a substring by extracting elements from another string
+# indexing[] / slice()
+# [start:stop:step]
+
+name = 'bro code'
+
+first_name = name[0]   # only printing a letter 
+first_name = name[0:7] # displaying a word ([:7])
+last_name = name[8:14] # displays last name ([8:])
+funky_name = name[0:8:3] # this one make no sence
+reversed_name = name[::-1] # reversing a string
+
+print(reversed_name)
