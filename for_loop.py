f957fa0c12f27e7e1073ea5cbfe5715bd61e2cdd
RavinduPerera <it22064622@my.sliit.lk>
2024-07-14 23:40:27 +0530

    for loop done

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+# for - limited and while - unlimited 
+
+# display one range 
+for i in range(10+1): # i - index
+    print(i)
+
+# display a specific range 
+for i in range(10, 50+1): # no 1 - inclusive, no 2 - exclusive
+    print(i)
+    print('')
+
+# we can change the x2 time 
+for i in range(10, 50+1, 2):
+    print(i)
+
+for i in 'Ravindu perera':
+    print(i)
+
