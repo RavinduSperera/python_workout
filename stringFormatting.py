7684691dc493c39c930a8e9cae030dfff7986117
RavinduPerera <it22064622@my.sliit.lk>
2024-07-23 00:33:29 +0530

    string formatting done

---
+++ stringFormatting.py
@@ -0,0 +1,19 @@
+# strings formatting
+
+height = 176
+name = 'Ravindu Perera'
+
+# method 1 - C type formatting"""
+"""message = 'Hello %s, Your heigjt is %d.' %(name, height)"""
+
+# method 2 - .format() - can do indexing as well 
+# name = 0 and height = 1 (indexes)
+"""message = 'Hello {}, your height is {}'.format(name, height)"""
+
+# method 3 - f string formatting - recomended
+"""message = f'Hello {name}, Your height is {height}'"""
+
+# method 4 - general way
+message = 'Hello '+name+' Your height is '+str(height)
+
+print(message)
