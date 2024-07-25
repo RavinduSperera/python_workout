2767a1d246a5ed6e16959becba8f08e6b82d6d0c
RavinduPerera <it22064622@my.sliit.lk>
2024-07-25 19:49:00 +0530

    file handling project done
    but output was wrong. fix it!!

--- files_project.py
+++ files_project.py
@@ -1,6 +1,11 @@
-# highest marks for each subject - done
-# height total mark holder of the data set
+# Task 1 - highest marks for each subject - done
+# Task 2 - height total mark holder of the data set - done but not correct
 
+# mapper function **
+def get_marks(record: tuple):
+    return record[1]
+
+# function to find the max marks holder student for each subject 
 def get_top_student(subject: str, dataset: dict):
     max_marks = 0
     max_student = ''
@@ -24,7 +29,10 @@ if not lines:
 marks_lines = lines[1:] # from line 1 to end line
 
 subject_marks = {
-    # a dictionary
+    # a dictionary for task 1
+}
+student_marks = {
+    # a dictionary for task 2 
 }
 
 for line in marks_lines:
@@ -41,8 +49,49 @@ for line in marks_lines:
     
     subject_marks[subject][name] = marks
 
+    prev_marks = student_marks.get(name, 0)
+    student_marks[name] = prev_marks + marks
+
+messages = [] # creating a list for write file
+
 # function calling 
 for subject, dataset in subject_marks.items():
     name, marks = get_top_student(subject, dataset)
     msg = f'Top student for {subject} is {name} with {marks}'
     print(msg)
+    messages.append(msg)
+
+# print(student_marks)
+
+marks_list = [ (name, marks) for name, marks in student_marks.items()]
+
+marks_list.sort(key=get_marks, reverse=True)
+top = marks_list[0]
+msg = f'Top student is {top[0]} with {top[1]} marks.'
+messages.append(msg)
+print('') # this is for a space between tasks
+print(msg)
+
+with open('results.txt', 'w') as output_file:
+    for msg in messages:
+        output_file.write(msg)
+        output_file.write('\n')
+
+
+
+
+
+
+# issue
+"""
+Something is wrong with this program. The calculations were not properly implemented. 
+recheck **
+
+the answer should be 
+    Top student for Science is Kusum with 90
+    Top student for Sinhala is Kelum with 96
+    Top student for Math is Praneeth with 85
+
+    Top student is Kusum with 211 marks.
+
+"""
